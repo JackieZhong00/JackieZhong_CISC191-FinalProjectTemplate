0c4a2c3f06280279be7fcc8ef3ff569447572f83
JackieZhong00 <jackiery.zhong@gmail.com>
2024-07-02 13:19:10 -0700

    version 1.0.0 completed mvp

--- Server/src/main/java/edu/sdccd/cisc191/template/Server.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/Server.java
@@ -3,12 +3,12 @@ package edu.sdccd.cisc191.template;
 
 import java.io.*;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.*;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -26,62 +26,56 @@ import javafx.application.Application;
 
 
 /**
- * This program is a server that takes connection requests on
- * the port specified by the constant LISTENING_PORT.  When a
- * connection is opened, the program sends the current time to
- * the connected socket.  The program will continue to receive
- * and process connections until it is killed (by a CONTROL-C,
- * for example).  Note that this server processes each connection
- * as it is received, rather than creating a separate thread
- * to process the connection.
+ * This is Server/Root class that represents the Study Tool application,
+ * which allows users to pass in files with test questions and answers
+ * that they want to use to test themselves with.
  */
 public class Server extends Application {
-//    private ServerSocket serverSocket;
-//    private Socket clientSocket;
-//    private PrintWriter out;
-//    private BufferedReader in;
 
-//    public void start(int port) throws Exception {
-//        serverSocket = new ServerSocket(port);
-//        clientSocket = serverSocket.accept();
-//        out = new PrintWriter(clientSocket.getOutputStream(), true);
-//        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-//
-//        String inputLine;
-//        while ((inputLine = in.readLine()) != null) {
-//            CustomerRequest request = CustomerRequest.fromJSON(inputLine);
-//            CustomerResponse response = new CustomerResponse(request.getId(), "Jane", "Doe");
-//            out.println(CustomerResponse.toJSON(response));
-//        }
-//    }
-
-//    public void stop() throws IOException {
-//        in.close();
-//        out.close();
-//        clientSocket.close();
-//        serverSocket.close();
-//    }
-
+    /**
+     *
+     * @param stage is window used to display GUI of application
+     * starts up the application in a new window
+     */
     public void start(Stage stage) {
-        Label message = new Label
-                ("Study tool - To use: submit a .txt file with questions and answers that you want to quiz yourself with. " +
+        Label instructions = new Label
+                ("To use: submit a .txt file w/ questions and answers that you want to quiz yourself with. \n " +
                         "File must be formatted like this: ");
-        message.setFont( new Font(20) );
-        message.setStyle("-fx-font-weight: bold");
-        message.setStyle("-fx-display: block");
-        Label fileFormatDemoLine1 = new Label("Topic: Recursion");
-        Label fileFormatDemoLine2 = new Label("Question1 Answer1 Question2 Answer2");
-        fileFormatDemoLine1.setFont( new Font(20));
-        fileFormatDemoLine2.setFont( new Font(20));
-        fileFormatDemoLine1.setStyle(" -fx-display: block");
-        fileFormatDemoLine2.setStyle(" -fx-display: block");
-        VBox fileDemoVBox = new VBox(50, fileFormatDemoLine1, fileFormatDemoLine2);
+        instructions.setFont( new Font(20) );
+        instructions.setStyle("-fx-font-weight: bold");
+        instructions.setStyle("-fx-display: block");
+
+        //label that demonstrates to user how to format file for submission
+        Label fileFormatDemoLine = new Label("Topic: Recursion \n" + "Question1\n" + "Answer1\n" +
+                "Topic: Data Structures\n"+"Question2\n" + "Answer2\n");
+        //set Font, display, and margin property for fileFormatDemo label
+        fileFormatDemoLine.setFont( new Font(18));
+        fileFormatDemoLine.setStyle(" -fx-display: block");
+        VBox fileDemoVBox = new VBox(fileFormatDemoLine);
+        VBox.setMargin(fileDemoVBox, new Insets(25, 0, 0, 0));
+
+        //visit next quiz question and reveal answer buttons
+        Button nextButton = new Button("Start/Next");
+        Button revealButton = new Button("Reveal Answer");
+
+        //empty label that will later on be assigned quiz question/answer
+        Label questionAnswerLabel = new Label(" ");
+        questionAnswerLabel.setFont(new Font(18));
+        VBox questionAnswerVBox = new VBox(questionAnswerLabel);
+        VBox.setMargin(questionAnswerVBox, new Insets(100, 0, 100, 0));
+        questionAnswerVBox.setStyle("-fx-border-color: black");
+        questionAnswerLabel.setPrefHeight(300.0);
+
+        //button to allow user to submit file with questions and answers
+        Button submitFileButton = new Button("Submit a File");
+        HBox buttonBar = new HBox( 20, submitFileButton, nextButton, revealButton );
+        buttonBar.setAlignment(Pos.CENTER);
+
 
         //sorting comes in useful for searching purposes later on
         TreeMap<String, ArrayList<Question>> treeMap = new TreeMap<>();
 
 
-        Button submitFileButton = new Button("Submit a File");
 
         submitFileButton.setOnAction(e -> {
             treeMap.clear();
@@ -92,32 +86,38 @@ public class Server extends Application {
                 Scanner scanner = new Scanner(file);
                 while(scanner.hasNextLine()) {
                     String[] inputArray = scanner.nextLine().split(" ");
-                    if(inputArray[0].equals("Topic ")) {
+                    System.out.println(inputArray[0]);
+                    System.out.println(inputArray[1]);
+                    if(inputArray[0].equals("Topic:")) {
                         //this is the key portion of the key-value pair to be added to TreeMap
                         String topic = inputArray[1];
-                        //array that holds all question objects, will be the value assigned to they key
-                        ArrayList<Question> questions = new ArrayList<>();
-                        //array to hold list of questions and answers
-                        String[] listOfQuestions = scanner.nextLine().split(" ");
-                        for(int i = 0; i < listOfQuestions.length; i+=2) {
-                            if (i+1 < listOfQuestions.length) {
-                                Question newQuestion = new Question(listOfQuestions[i],listOfQuestions[i+1]);
-                                questions.add(newQuestion);
-                            }
+                        //array that holds the Question objects
+                        if(treeMap.containsKey(topic)) {
+                            ArrayList<Question> questionArr = treeMap.get(topic);
+                            String questionLine = scanner.nextLine();
+                            String answerLine = scanner.nextLine();
+                            questionArr.add(new Question(questionLine, answerLine));
+                            treeMap.put(topic, questionArr);
                         }
-                        //insert the key-value pair into TreeMap
-                        treeMap.put(topic, questions);
+                        else {
+                            String questionLine = scanner.nextLine();
+                            String answerLine = scanner.nextLine();
+                            Question question = new Question(questionLine, answerLine);
+                            ArrayList<Question> questionArr = new ArrayList<>();
+                            questionArr.add(question);
+                            treeMap.put(topic, questionArr);
+                        }
                     }
+                    else {
+                        System.out.println("Incorrect format detected");
+                    }
+                    questionAnswerLabel.setText("press start/next button to start quiz");
                 }
             } catch (FileNotFoundException error) {
                 System.out.println("File not found");
             }});
 
-        Button nextButton = new Button("Start/Next");
-        Button revealButton = new Button("Reveal Answer");
-        Label questionAnswerLabel = new Label(" ");
-        HBox buttonBar = new HBox( 20, submitFileButton, nextButton, revealButton );
-        buttonBar.setAlignment(Pos.CENTER);
+
 
 
         nextButton.setOnAction(e -> {
@@ -125,11 +125,11 @@ public class Server extends Application {
             if (!treeMap.isEmpty()) {
                 ArrayList<String> keyList= new ArrayList<>(treeMap.keySet());
                 Random random = new Random();
-                String key = keyList.get(random.nextInt());
+                String key = keyList.get(random.nextInt(keyList.size()));
                 //use the key to get list of Question objects from treeMap
                 ArrayList<Question> questions = treeMap.get(key);
                 //generate a random int to get random Question from list of Questions
-                Question quizQuestion = questions.get(random.nextInt());
+                Question quizQuestion = questions.get(random.nextInt(questions.size()));
                 //set questionAnswerLabel to the question
                 questionAnswerLabel.setText(quizQuestion.getQuestion());
                 //upon pressing reveal button, set questionAnswerLabel to answer
@@ -139,27 +139,16 @@ public class Server extends Application {
             }
         });
 
-        BorderPane root = new BorderPane();
-        root.setTop(message);
-        root.setCenter(fileDemoVBox);
-        root.setCenter(questionAnswerLabel);
-        root.setBottom(buttonBar);
+        VBox root = new VBox(instructions, fileDemoVBox, questionAnswerVBox, buttonBar);
 
-        Scene scene = new Scene(root, 600, 600);
+
+        Scene scene = new Scene(root, 750, 750);
         stage.setScene(scene);
         stage.setTitle("Study Tool");
         stage.show();
     }
 
     public static void main(String[] args) {
-//        Server server = new Server();
-//        try {
-//            server.start(4444);
-//            server.stop();
-//        } catch(Exception e) {
-//            e.printStackTrace();
-//        }
         launch();
-
     }
 } //end class Server
